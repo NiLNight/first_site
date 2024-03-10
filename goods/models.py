a6fb4e7f8209540c20cc9e48695a8d57f37ba745
NiLNight <bkmz-2020@inbox.ru>
2024-03-10 13:41:00 +0300

    фикстуры

--- goods/models.py
+++ goods/models.py
@@ -20,8 +20,8 @@ class Products(models.Model):
     description = models.TextField(blank=True, null=True, verbose_name='Описание')
     image = models.ImageField(upload_to='goods_images', blank=True, null=True, verbose_name='Изображение')
     price = models.DecimalField(default=0.00, max_digits=7, decimal_places=2, verbose_name='Цена')
-    discount = models.DecimalField(default=0.00, max_digits=7, decimal_places=2, verbose_name='Скидка')
-    quantity = models.PositiveIntegerField(default=True, verbose_name='Количество')
+    discount = models.DecimalField(default=0.00, max_digits=4, decimal_places=2, verbose_name='Скидка')
+    quantity = models.PositiveIntegerField(default=0, verbose_name=' Количество')
     category = models.ForeignKey(to=Categories, on_delete=models.CASCADE, verbose_name='Категория')
 
     class Meta:
