3e55295d707699cc37d6330b9712972411a69a92
NiLNight <bkmz-2020@inbox.ru>
2024-06-22 20:13:07 +0300

    новое приложение users

--- goods/utils.py
+++ goods/utils.py
@@ -2,6 +2,7 @@ from goods.models import Products
 from django.db.models import Q
 from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank, SearchHeadline
 
+
 def q_search(query):
     if query.isdigit() and len(query) <= 5:
         return Products.objects.filter(id=int(query))
