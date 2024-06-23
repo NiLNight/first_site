078c42a35be93b2b6acb8bc96de87eb7b6e2a024
NiLNight <bkmz-2020@inbox.ru>
2024-06-23 19:58:53 +0300

    новое приложение users

--- users/models.py
+++ users/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class User(AbstractUser):
     image = models.ImageField(upload_to='users_images',
                               blank=True, null=True, verbose_name='Аватар')
+
     class Meta:
         db_table = 'user'
         verbose_name = 'Пользователя'
