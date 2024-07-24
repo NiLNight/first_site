5263d2640f60410e4b7e2122a78d6daf2a4a66dd
NiLNight <bkmz-2020@inbox.ru>
2024-07-24 21:02:14 +0300

    добавлена валидация номера телефона

--- orders/forms.py
+++ orders/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+import re
 
 
 class CreateOrderForm(forms.Form):
@@ -15,7 +16,17 @@ class CreateOrderForm(forms.Form):
         ('1', 'True'),
     ])
 
-    # first_name = forms.CharField(
+    def clean_phone_number(self):
+        data = self.cleaned_data['phone_number']
+        if not data.isdigit():
+            raise forms.ValidationError('Номер телефона должен содержать только цифры')
+
+        pattern = re.compile(r'^\d{10}')
+        if not pattern.match(data):
+            raise forms.ValidationError('Неверный формат номера')
+        return data
+
+        # first_name = forms.CharField(
     #     widget=forms.TextInput(
     #         attrs={'class': 'form-control',
     #                'placeholder': 'Введите ваше имя',
