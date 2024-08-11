12b542452ba3a5426868cbda8b7ad69d4bf0944a
NiLNight <bkmz-2020@inbox.ru>
2024-08-11 19:41:01 +0300

    представления на основе классов

--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@ from django.contrib import auth, messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView
+from django.core.cache import cache
 from django.db.models import Prefetch
 from django.http import HttpResponseRedirect
 from django.shortcuts import redirect
@@ -9,7 +10,9 @@ from django.urls import reverse, reverse_lazy
 from django.views.generic import TemplateView
 from django.views.generic.edit import CreateView, UpdateView
 
+import orders
 from carts.models import Cart
+from common.mixins import CacheMixins
 from orders.models import OrderItem, Order
 from users.forms import UserLoginForm, UserRegisterForm, ProfileForm
 
@@ -125,7 +128,7 @@ class UserRegisterView(CreateView):
 #     return render(request, 'users/registration.html', context)
 
 
-class UsrProfileView(LoginRequiredMixin, UpdateView):
+class UsrProfileView(LoginRequiredMixin, CacheMixins, UpdateView):
     template_name = 'users/profile.html'
     form_class = ProfileForm
     success_url = reverse_lazy('users:profile')
@@ -140,12 +143,15 @@ class UsrProfileView(LoginRequiredMixin, UpdateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = "Home - Кабинет"
-        context['orders'] = Order.objects.filter(user=self.request.user).prefetch_related(
+
+        orders = Order.objects.filter(user=self.request.user).prefetch_related(
             Prefetch(
                 'orderitem_set',
-                queryset=OrderItem.objects.select_related('product')
+                queryset=OrderItem.objects.select_related('product'),
             )
         ).order_by('-id')
+
+        context['orders'] = self.set_get_cache(orders, f'user_{self.request.user.id}_orders', 60 * 2)
         return context
 
     def form_invalid(self, form):
